b7b9b9fa0932e31b1c6a060db65bf4b152896564
braian reising <bra.reising@gmail.com>
2024-12-03 15:50:27 -0300

    clase dados terminada con su juego y metodos

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Casino } from "./poo/Casino";
+import { Dado } from "./poo/Dado";
 /* import { Digital } from "./poo/Digital"; */
 // import { Analogico } from "./poo/Analogico";
 // import { Dado } from "./poo/Dado";
@@ -8,11 +9,11 @@ import { Ruleta } from "./poo/Ruleta";
 const casino: Casino = new Casino("Casino 'La suerte'");
 
 // CREACION DE JUEGOS
-/* const digital: Digital = new Digital("digital", 20, 10); */
-// const analogico: Analogico = new Analogico();
-// const dado: Dado = new Dado("dados", 20, 2000,10);
+ /*const digital: Digital = new Digital("digital", 20, 10); */
+ /* const analogico: Analogico = new Analogico(); */
+ // Crear instancia del juego de dados
+const juegoDado = new Dado("Dados", 10, 50, 2, 8); // Apuesta mínima: 10, máxima: 50, 2 dados, meta: 8
 const ruleta: Ruleta = new Ruleta();
 casino.agregarJuego(ruleta);
-// casino.agregarJuego(dado);
+ casino.agregarJuego(juegoDado);
 casino.menuPrincipal();
-
